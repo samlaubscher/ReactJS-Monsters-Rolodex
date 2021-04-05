47c785a1756e804380553e842554d53ee8f5a717
samlaubscher <hakaewt@protonmail.com>
2021-04-05 23:36:24 +0100

    Update files for github pages deployment

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
-  onSearchChange = (e) => {
+  handleChange = (e) => {
     this.setState({ searchField: e.target.value });
   };
 
@@ -33,9 +33,10 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
-          onSearchChange={this.onSearchChange}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
